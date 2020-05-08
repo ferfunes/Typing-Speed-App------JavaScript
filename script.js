1e0910b31c65c11f31b4b2f3caad24a8aa01037b
ferfunes <ferfunezdev@gmail.com>
2020-05-08 14:05:37 -0400

    added reset functionality

--- script.js
+++ script.js
@@ -5,40 +5,67 @@ const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
 let timer = [0, 0, 0, 0];
+let interval;
+let timerRunning = false;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
+function leadingZero(time) {
+  if (time <= 9) {
+    time = "0" + time;
+  }
+  return time;
+}
 
 // Run a standard minute/second/hundredths timer:
 function runTimer() {
-  theTimer.innerHTML = timer;
-  timer++;
+  let currentTime = `${leadingZero(timer[0])}:${leadingZero(
+    timer[1]
+  )}:${leadingZero(timer[2])}`;
+  theTimer.innerHTML = currentTime;
+  timer[3]++;
+
+  timer[0] = Math.floor(timer[3] / 100 / 60);
+  timer[1] = Math.floor(timer[3] / 100 - timer[0] * 60);
+  timer[2] = Math.floor(timer[3] - timer[1] * 100 - timer[0] * 6000);
 }
+
 // Match the text entered with the provided text on the page:
 function spellCheck() {
   let textEntered = testArea.value;
+  let originTextMatch = originText.substring(0, textEntered.length);
 
-  if (textEntered === originText) {
+  if (originTextMatch === textEntered) {
     testWrapper.style.borderColor = "blue";
-  } else {
+  }
+  if (originTextMatch !== textEntered) {
     testWrapper.style.borderColor = "red";
   }
-  console.log(textEntered);
+  if (originText === textEntered) {
+    testWrapper.style.borderColor = "green";
+
+    clearInterval(interval);
+  }
 }
 
 // Start the timer:
 function start() {
   let textEnteredLength = testArea.value.length;
 
-  if (textEnteredLength === 0) {
-    setInterval(runTimer, 10);
+  if (textEnteredLength === 0 && !timerRunning) {
+    timerRunning = true;
+    interval = setInterval(runTimer, 10);
   }
-
-  console.log(textEnteredLength);
 }
 
 // Reset everything:
 function reset() {
-  console.log("clicked");
+  clearInterval(interval);
+  interval = null;
+  timer = [0, 0, 0, 0];
+  testArea.value = "";
+  theTimer.innerHTML = "00:00:00";
+  timerRunning = false;
+  testWrapper.style.borderColor = "gray";
 }
 
 // Event listeners for keyboard input and the reset button:
