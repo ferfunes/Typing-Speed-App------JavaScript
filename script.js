d23bab5c01134f2c53b3e48c0d5abc8be2ef6d66
ferfunes <ferfunezdev@gmail.com>
2020-05-08 11:12:28 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
+const theTimer = document.querySelector(".timer");
+
+// Add leading zero to numbers 9 or below (purely for aesthetics):
+
+// Run a standard minute/second/hundredths timer:
+
+// Match the text entered with the provided text on the page:
+
+// Start the timer:
+
+// Reset everything:
+
+// Event listeners for keyboard input and the reset button:
+testArea.addEventListener("keydown", function (e) {
+  console.log(testArea.value);
+  console.log(e);
+});
+
+resetButton.addEventListener("click", function (e) {
+  console.log("clicked");
+});
