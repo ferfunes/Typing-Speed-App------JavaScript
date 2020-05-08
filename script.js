6d7ca561b776dec542ac83d255fba891b732d35f
ferfunes <ferfunezdev@gmail.com>
2020-05-08 11:57:58 -0400

    added start, spellCheck and runTimer functions

--- script.js
+++ script.js
@@ -4,22 +4,45 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+let timer = [0, 0, 0, 0];
+
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 // Run a standard minute/second/hundredths timer:
-
+function runTimer() {
+  theTimer.innerHTML = timer;
+  timer++;
+}
 // Match the text entered with the provided text on the page:
+function spellCheck() {
+  let textEntered = testArea.value;
+
+  if (textEntered === originText) {
+    testWrapper.style.borderColor = "blue";
+  } else {
+    testWrapper.style.borderColor = "red";
+  }
+  console.log(textEntered);
+}
 
 // Start the timer:
+function start() {
+  let textEnteredLength = testArea.value.length;
+
+  if (textEnteredLength === 0) {
+    setInterval(runTimer, 10);
+  }
+
+  console.log(textEnteredLength);
+}
 
 // Reset everything:
+function reset() {
+  console.log("clicked");
+}
 
 // Event listeners for keyboard input and the reset button:
-testArea.addEventListener("keydown", function (e) {
-  console.log(testArea.value);
-  console.log(e);
-});
+testArea.addEventListener("keypress", start);
+testArea.addEventListener("keyup", spellCheck);
 
-resetButton.addEventListener("click", function (e) {
-  console.log("clicked");
-});
+resetButton.addEventListener("click", reset);
